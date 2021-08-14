dd72ebf562522a512a3c3089fc5277783982ace2
Aivars Smausktelis <aivars.smaukstelis@gmail.com>
2021-08-14 15:40:36 +0300

    builder demo

---
+++ src/main/java/examples/builder/BuilderDemo.java
@@ -0,0 +1,24 @@
+package examples.builder;
+
+public class BuilderDemo {
+
+    //not good idea - mixing builder and javabean
+    //better - use builder to create immutable classes
+
+    public static void main(String[] args) {
+        simpleBuilder();
+    }
+
+    private static void simpleBuilder() {
+
+        var sandwichBuilder = new SandwichWithBuilder.Builder("no bread");
+        sandwichBuilder
+                .sauce("mustard")
+                .filling("chickpea");
+
+        var sandwich1 = sandwichBuilder.build();
+
+        System.out.println("got sandwich:{}"+sandwich1);
+
+    }
+}
---
+++ src/main/java/examples/builder/SandwichWithBuilder.java
@@ -0,0 +1,73 @@
+package examples.builder;
+
+public class SandwichWithBuilder {
+
+    public static class Builder {
+        private String bread;
+        private String sauce;
+        private String salad;
+        private String filling;
+
+        public Builder(String bread) {
+            //builder with enforced mandatory value
+            this.bread = bread;
+        }
+
+        public Builder sauce(String sauce) {
+            this.sauce = sauce;
+            return this;
+        }
+
+        public Builder salad(String salad) {
+            this.salad = salad;
+            return this;
+        }
+
+        public Builder filling(String filling) {
+            this.filling = filling;
+            return this;
+        }
+
+        public SandwichWithBuilder build() {
+            return new SandwichWithBuilder(this);
+        }
+    }
+
+    private final String bread;
+    private final String sauce;
+    private final String salad;
+    private final String filling;
+
+    private SandwichWithBuilder(Builder builder) {
+        this.bread = builder.bread;
+        this.sauce = builder.sauce;
+        this.salad = builder.salad;
+        this.filling = builder.filling;
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public String getSauce() {
+        return sauce;
+    }
+
+    public String getSalad() {
+        return salad;
+    }
+
+    public String getFilling() {
+        return filling;
+    }
+
+    @Override
+    public String toString() {
+        return "SandwichWithBuilder{" +
+                "bread='" + bread + '\'' +
+                ", sauce='" + sauce + '\'' +
+                ", salad='" + salad + '\'' +
+                ", filling='" + filling + '\'' +
+                '}';
+    }
+}
